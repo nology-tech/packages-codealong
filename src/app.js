fb7c1e5a15e5917a0121a0928616581b0c1df82a
charlie-robin <charlie.robin.richardson@gmail.com>
2022-05-26 09:06:15 +0100

    Small tweak so background color is set if the image has already loaded, avoid caching :S

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const randomInRange = (min, max) => {
   return Math.random() * (max - min) + min;
 };
 
-const fireConfetti = (colorPalette) => {
+const fireConfetti = colorPalette => {
   const confettiOptions = {
     particleCount: randomInRange(50, 100),
     angle: randomInRange(55, 125),
@@ -38,10 +38,14 @@ const onImageLoad = () => {
   fireConfetti(colorPalette);
 };
 
-const handleUrlInput = (event) => {
+if (dogImage.complete) {
+  onImageLoad();
+}
+
+const handleUrlInput = event => {
   dogImage.src = event.target.value;
 };
 
 confettiButton.addEventListener("click", handleConfettiPress);
-dogImage.addEventListener("load", onImageLoad);
 imageUrlInput.addEventListener("input", handleUrlInput);
+dogImage.addEventListener("load", onImageLoad);
